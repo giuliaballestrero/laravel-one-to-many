3f5e4575e7b42f98ada352d81f14c82cc8a80178
giuliaballestrero <giulia.ballestrero@gmail.com>
2023-02-28 16:49:40 +0100

    Update databaseSeeder and projectsTableSeeder

--- database/seeders/ProjectsTableSeeder.php
+++ database/seeders/ProjectsTableSeeder.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Faker\Generator as Faker;
 use App\Models\Project;
+use App\Models\Type;
 use Illuminate\Support\Str;
 
 class ProjectsTableSeeder extends Seeder
@@ -19,12 +20,12 @@ class ProjectsTableSeeder extends Seeder
     {
         for ($i=0; $i < 30; $i++) {
             $newProject = new Project();
+            $newProject->category_id = Type::inRandomOrder()->first()->id ;
             $newProject->title = $faker->unique()->realTextBetween(5, 30);
             $newProject->slug = Str::slug($newProject->title);
             $newProject->description = $faker->realText(100);
             $newProject->thumb = $faker->imageUrl();
             $newProject->creation_date = $faker->dateTimeThisYear();
-            $newProject->type = $faker->realTextBetween(5,10);
             $newProject->completed = $faker->randomElement([true, false]);
             $newProject->save();
         }
